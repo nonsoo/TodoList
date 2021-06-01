111e1bafc343cb76e1e2d07050b43f6d1fa00cff
Nonso <nonsootoh@gmail.com>
2021-05-31 23:56:40 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nonso Otoh on 2021-05-31.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
